c55a1e145998a479be136658a072e2f48eedf2a8
karinaverma7 <129973522+karinaverma7@users.noreply.github.com>
2024-09-24 13:35:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+
+import streamlit as st
+import pickle
+import requests
+import pandas as pd
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+       
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names
+
+st.header('Movie Recommender System')
+movies = pickle.load(open('movie_list.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+if st.button('Recommend'):
+    recommendations= recommend(selected_movie)
+    for i in recommendations:
+        st.write(i)
